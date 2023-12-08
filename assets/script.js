509bc159a07d96b00b1f557f23b1335983007699
Mitchell <tmitchell@cdsvisual.com>
2023-12-08 15:52:50 -0700

    starting files, API keys

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+5dayAPIkey ='5a48e8bfa31d3c2a506e7232ebe1fe5c'
+geocodingAPIkey = '5a48e8bfa31d3c2a506e7232ebe1fe5c'
+geocoding = 'http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={geocodingAPIkey}'
+5dayForcast = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={5dayAPIkey}'
